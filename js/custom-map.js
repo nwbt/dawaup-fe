6df47fb68be2ca1bad3db8ee17c01f4ca33480c9
Dan Catalano <dev@nwbt.co>
2017-01-09 23:33:50 -0700

    split elements (finally)
    map and sidebar are now floating

--- js/custom-map.js
+++ js/custom-map.js
@@ -6,6 +6,36 @@
  */
 (function(){
   'use strict';
-  
-    
+	console.log('here')
+		var montanaCoord = [-110, 47];
+
+		var mapSource = new ol.source.Vector({
+				format: new ol.format.GeoJSON(),
+				url: "data/MontanaNetworkLatLon.geojson"
+		});
+
+		var vectorLayer = new ol.layer.Vector({
+				source: mapSource
+		});
+
+		var rasterLayer = new ol.layer.Tile({
+				source: new ol.source.Stamen({
+						layer: 'terrain-background'
+				})
+		});
+
+		var mapLayers = [rasterLayer, vectorLayer];
+
+		var mapView = new ol.View({
+				projection: 'EPSG:4326',
+				center: montanaCoord,
+				zoom: 7
+		});
+
+		var montanaMap = new ol.Map({
+				target: 'map-floatleft',
+				layers: mapLayers,
+				view: mapView
+		});
+	console.log('here')
 })();
